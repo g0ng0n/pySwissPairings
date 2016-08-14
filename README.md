78291f0e2f4134fd95fcb99aff57bfd9dbea826e
Gonzalo Gisbert <gonzalo.a.gisbert@gmail.com>
2016-08-13 21:40:40 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Project: Tournament Results
+
+## Description
+    In this project is a Python module that uses the PostgreSQL database to keep track of players and matches in a game tournament.
+    The module will use the Swiss system for pairing up players in each round: 
+    players are not eliminated, each player should be paired with another player with the same number of wins, or as close as possible.
+    This project has two parts: defining the database schema (SQL table definitions), and writing the code that will use it.
+
+
+## Understand the purpose of each file
+ * tournament.sql  - this file is used to set up your database schema (the table representation of your data structure).
+ * tournament.py - this file is used to provide access to your database via a library of functions which can add, delete or query data in your database to another python program (a client program). Remember that when you define a function, it does not execute, it simply means the function is defined to run a specific set of instructions when called.
+ * tournament_test.py - this is a client program which will use your functions written in the tournament.py module. We've written this client program to test your implementation of functions in tournament.py
+
+
+## Running the project!
+ * Prerequisities: python2.7, PostgreSQL(psql (9.3.13)
+ * To run the series of tests defined in this test suite, run the program from the command line 
+    $ python tournament_test.py 
+
+
+
+
---
+++ tournament.sql
@@ -0,0 +1,57 @@
+-- Table definitions for the tournament project.
+--
+-- Put your SQL 'create table' statements in this file; also 'create view'
+-- statements if you choose to use it.
+--
+-- You can write comments in this file by starting them with two dashes, like
+-- these lines here.
+
+-- Revoking connection from multiple users in order to drop the table if EXIST
+SELECT
+    pg_terminate_backend(pid)
+FROM
+    pg_stat_activity
+WHERE
+    -- don't kill my own connection!
+    pid <> pg_backend_pid()
+    -- don't kill the connections to other databases
+    AND datname = 'tournament'
+    ;
+
+DROP DATABASE IF EXISTS tournament;
+CREATE DATABASE tournament;
+
+DROP TABLE IF EXISTS players CASCADE;
+
+CREATE TABLE players (
+    id serial,
+    name varchar,
+    wins integer,
+    losses integer,
+    PRIMARY KEY(id)
+);
+
+DROP TABLE IF EXISTS matches CASCADE;
+
+CREATE TABLE matches (
+    match_id serial,
+    winner_id serial references players(id) ON DELETE CASCADE,
+    losser_id serial references players(id) ON DELETE CASCADE,
+    primary key(match_id)
+);
+
+-- Creates a view of the standings:
+
+Create view Standings AS
+select players.id, players.name,
+    (Select count(matches.winner_id)
+from matches
+    Where players.id = matches.winner_id)
+AS total_wins,
+    (select count(matches.match_id)
+FROM matches
+    where players.id = matches.winner_id
+    or players.id = matches.losser_id)
+As total_matches
+    from players
+Order by total_wins DESC, total_matches DESC
